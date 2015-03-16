15962d582a36f7794fc4af65c4e228c88d5e6739
Iain Ballard <iain.ballard@gmail.com>
2015-03-16 13:18:45 +0000

    failing

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ var global, exports;
 
 (function (provides) {
 
-
+    provides.fibonacci = {
+        encode: function(i){return i;},
+        decode: function(i){return i;}
+    }
 
 /* istanbul ignore next */ // `this` branch doesn't get followed
 })(global || exports || this);
--- test/fibonacci_tests.js
+++ test/fibonacci_tests.js
@@ -1,10 +1,21 @@
 var expect = require('chai').expect;
 
-var tree = require("../index.js");
+var fib = require("../index.js").fibonacci;
 
-describe("Given", function() {
-    describe("When", function() {
-        it("should", function(){
+describe("Using the fibonacci encoder", function() {
+    describe("When encoding and decoding arrays of natural numbers", function() {
+        it("should result in the original input after an encode and decode", function(){
+            var input_array = [1,2,3,4,5,100, 200];
+            var output = fib.decode(fib.encode(input_array));
+
+            expect(output).to.equal(input_array);
+        });
+
+        it("should produce a single buffer as output", function(){
+            var output = fib.encode([1,2,3]);
+
+            expect(output).to.be.an.instanceof(Buffer);
+
         });
     });
 });
